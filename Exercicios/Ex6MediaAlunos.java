9becb97a9b8632a66e4d4e11c435308c20a664fe
Samuel Sales <samuelsales943@gmail.com>
2026-04-18 20:02:16 -0300

    ex6 done

---
+++ Exercicios/Ex6MediaAlunos.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ex6MediaAlunos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] vetorMedias = new double[2];
+        double[] vetorNotas = new double[4];
+        
+        for(int i = 0; i < vetorMedias.length; i++) {
+            double soma = 0;
+
+            for(int nota = 0; nota < vetorNotas.length; nota++) {
+                System.out.println("Digite sua nota " + (nota + 1) + ": ");
+                vetorNotas[nota] = sc.nextDouble();
+                soma += vetorNotas[nota];
+            }
+
+            vetorMedias[i] = soma / 4;
+
+        }
+
+        System.out.println("--- ALUNOS APROVADOS ---");
+        for (int i = 0; i < vetorMedias.length; i++) {
+            if(vetorMedias[i] >= 7.0) {
+                System.out.println("ALUNO " + (i + 1) + " (APROVADO) | Média: " + vetorMedias[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
